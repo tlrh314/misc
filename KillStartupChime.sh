82579c888881d8a5ab6089daffac6507ae0c25cf
tlrh314 <timohalbesma@gmail.com>
2014-03-13 16:16:17 +0100

    Changed  and

--- KillStartupChime.sh
+++ KillStartupChime.sh
@@ -1,9 +1,10 @@
-#! /bin/bash
+#!/bin/bash
 #kill the annoying startup chime' by Timo Halbesma, March 27 2012
 # timohalbesma@gmail.com
 
 ON_FILE="/Library/Scripts/mute-on.sh"
 OFF_FILE="/Library/Scripts/mute-off.sh"
+NOW=$(date +"%B %d, %Y")
 
 function remove() {
     if [ -e $ON_FILE ]; then
@@ -18,21 +19,25 @@ function remove() {
 function doinstall() {
     # Remove currect script if one exists
     remove
-    # Create the ON and OFF scripts
 
-    sudo sh -c "echo \"#! /bin/bash
-# Kill the annoying tartup chime by Timo Halbesma, March 27 2012
+# Create the ON and OFF scripts
+    sudo sh -c "echo \"#!/bin/bash
+# Kill the annoying tartup chime by Timo Halbesma, $NOW
 # timohalbesma@gmail.com
-        osascript -e 'set volume with output muted'\" >> $ON_FILE"
+    osascript -e 'set volume with output muted'\" >> $ON_FILE"
 
-        sudo sh -c "echo \"#! /bin/bash
-# Kill the annoying tartup chime by Timo Halbesma, March 27 2012
+    sudo sh -c "echo \"#!/bin/bash
+# Kill the annoying tartup chime by Timo Halbesma, $NOW
 # timohalbesma@gmail.com
-        osascript -e 'set volume without output muted'\" >> $OFF_FILE"
+    osascript -e 'set volume without output muted'\" >> $OFF_FILE"
 
-    # Chmod the ON and OFF scripts to +x
-    sudo chmod +x $ON_FILE
-    sudo chmod +x $OFF_FILE
+    # Chmod the ON and OFF scripts to +x to make then executable.
+    if [ -e $ON_FILE ]; then
+        sudo chmod +x $ON_FILE
+        sudo chmod +x $OFF_FILE
+    else
+        echo "Something went wrong!"
+    fi
 
     # Write the LoginHook and the LoginHook
     sudo defaults write com.apple.loginwindow LogoutHook $ON_FILE
